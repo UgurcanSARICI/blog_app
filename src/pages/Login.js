5da94d581892d82e192f4ad9d17689641e147f28
UgurcanSARICI <ugurcansarici@gmail.com>
2022-12-14 20:46:19 +0300

    login,new blog,profile

---
+++ src/pages/Login.js
@@ -0,0 +1,179 @@
+import * as React from "react";
+import Avatar from "@mui/material/Avatar";
+import Button from "@mui/material/Button";
+import CssBaseline from "@mui/material/CssBaseline";
+import TextField from "@mui/material/TextField";
+import FormControlLabel from "@mui/material/FormControlLabel";
+import Checkbox from "@mui/material/Checkbox";
+import Link from "@mui/material/Link";
+import Paper from "@mui/material/Paper";
+import Box from "@mui/material/Box";
+import Grid from "@mui/material/Grid";
+import LockOutlinedIcon from "@mui/icons-material/LockOutlined";
+import Typography from "@mui/material/Typography";
+import { createTheme, ThemeProvider } from "@mui/material/styles";
+
+import { useContext } from "react";
+import { AuthContext } from "../contexts/AuthContext";
+import { async } from "@firebase/util";
+import { useNavigate } from "react-router-dom";
+
+function Copyright(props) {
+  return (
+    <Typography
+      variant="body2"
+      color="text.secondary"
+      align="center"
+      {...props}
+    >
+      {"Copyright © "}
+      <Link color="inherit" href="https://ugurcansarici-portfolio.vercel.app/">
+        Your Website
+      </Link>{" "}
+      {new Date().getFullYear()}
+      {"."}
+    </Typography>
+  );
+}
+
+const theme = createTheme();
+
+export default function SignInSide() {
+  const navigate = useNavigate();
+
+  const { setEmail, setPassword, login, googleLogin } = useContext(AuthContext);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    login();
+  };
+  const googleSubmit = async () => {
+    await googleLogin();
+    window.location.replace("/");
+  };
+  return (
+    <ThemeProvider theme={theme}>
+      <Grid container component="main" sx={{ height: "100vh" }}>
+        <CssBaseline />
+        <Grid
+          item
+          xs={false}
+          sm={4}
+          md={7}
+          sx={{
+            backgroundImage: "url(https://source.unsplash.com/random)",
+            backgroundRepeat: "no-repeat",
+            backgroundColor: (t) =>
+              t.palette.mode === "light"
+                ? t.palette.grey[50]
+                : t.palette.grey[900],
+            backgroundSize: "cover",
+            backgroundPosition: "center",
+          }}
+        />
+        <Grid item xs={12} sm={8} md={5} component={Paper} elevation={6} square>
+          <Box
+            sx={{
+              my: 8,
+              mx: 4,
+              display: "flex",
+              flexDirection: "column",
+              alignItems: "center",
+            }}
+          >
+            <Avatar sx={{ m: 1, bgcolor: "secondary.main" }}>
+              <LockOutlinedIcon />
+            </Avatar>
+            <Typography component="h1" variant="h5">
+              Sign in
+            </Typography>
+            <Box
+              component="form"
+              noValidate
+              onSubmit={handleSubmit}
+              sx={{ mt: 1 }}
+            >
+              <TextField
+                margin="normal"
+                required
+                fullWidth
+                id="email"
+                label="Email Address"
+                name="email"
+                autoComplete="email"
+                autoFocus
+                onChange={(e) => setEmail(e.target.value)}
+              />
+              <TextField
+                margin="normal"
+                required
+                fullWidth
+                name="password"
+                label="Password"
+                type="password"
+                id="password"
+                autoComplete="current-password"
+                onChange={(e) => setPassword(e.target.value)}
+              />
+              <FormControlLabel
+                control={<Checkbox value="remember" color="primary" />}
+                label="Remember me"
+              />
+              <Button
+                type="submit"
+                fullWidth
+                variant="contained"
+                sx={{ mt: 3, mb: 2 }}
+              >
+                Sign In
+              </Button>
+              <Button
+                className="gap-5"
+                type="submit"
+                fullWidth
+                variant="contained"
+                sx={{ mt: 3, mb: 2 }}
+                onClick={googleSubmit}
+              >
+                <img
+                  className="w-[1.8rem]"
+                  src="https://cdn-icons-png.flaticon.com/512/2991/2991148.png"
+                  alt="google"
+                />
+                Google
+                <img
+                  className="w-[2rem]"
+                  src="https://iconarchive.com/download/i67980/marcus-roberto/google-play/Google-Chrome.ico"
+                  alt="google"
+                />
+              </Button>
+              <Grid container>
+                <Grid item xs>
+                  <Link href="#" variant="body2">
+                    Forgot password?
+                  </Link>
+                </Grid>
+                <Grid item>
+                  <Link
+                    onClick={() => navigate("/register")}
+                    to="/register"
+                    href="#"
+                    variant="body2"
+                  >
+                    {"Don't have an account? Sign Up"}
+                  </Link>
+                </Grid>
+              </Grid>
+            </Box>
+            <div className="flex justify-center items-center w-[20rem]  mt-14">
+              <img
+                src="https://pro2-bar-s3-cdn-cf.myportfolio.com/9bdefa6fb51a3e100907c6ce7b8d8b05/12f90cf5-d59b-4fa0-986e-8270e95c6a6a_rw_600.gif?h=4480998c4d68c54ee0ed10b802ae096a"
+                alt=""
+              />
+            </div>
+          </Box>
+        </Grid>
+      </Grid>
+    </ThemeProvider>
+  );
+}
